c4c51530abeb25ac43ee86118277fbb371fcd278
aruncs <acspayyeri@gmail.com>
2019-10-12 23:00:44 +0530

    bishop movement

--- src/client/js/bishop.js
+++ src/client/js/bishop.js
@@ -1,6 +1,34 @@
 import { piece } from "./piece"
+import { numberToFileNameMap, fileNameToNumberMap, isValidPosition } from "./util"
+
+const movements = [
+  { deltaFile: 1, deltaRank: 1 },
+  { deltaFile: 1, deltaRank: -1 },
+  { deltaFile: -1, deltaRank: 1 },
+  { deltaFile: -1, deltaRank: -1 }
+]
 
 const bishop = Object.create(piece)
-bishop.getPossibleMoves = function() {}
+bishop.getPossibleMoves = function() {
+  const currentFileNumber = fileNameToNumberMap[this.getFile()]
+  const currentRank = parseInt(this.getRank())
+  const possibleMoves = []
+
+  movements.forEach(movement => {
+    let rank = currentRank + movement.deltaRank
+    let fileNumber = currentFileNumber + movement.deltaFile
+    let file = numberToFileNameMap[fileNumber]
+    while (isValidPosition(file, rank)) {
+      possibleMoves.push({
+        file: file,
+        rank: rank
+      })
+      rank = rank + movement.deltaRank
+      fileNumber = fileNumber + movement.deltaFile
+      file = numberToFileNameMap[fileNumber]
+    }
+  })
+  return possibleMoves
+}
 
 export { bishop }
