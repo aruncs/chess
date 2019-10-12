90f1a786bde92d6219c1d8077642da92318568eb
aruncs <acspayyeri@gmail.com>
2019-10-12 23:33:35 +0530

    king movement

--- src/client/js/king.js
+++ src/client/js/king.js
@@ -1,6 +1,35 @@
 import { piece } from "./piece"
+import { numberToFileNameMap, fileNameToNumberMap, isValidPosition } from "./util"
 
+const movements = [
+  { deltaFile: 1, deltaRank: 1 },
+  { deltaFile: 1, deltaRank: 0 },
+  { deltaFile: -1, deltaRank: 1 },
+  { deltaFile: 0, deltaRank: -1 },
+  { deltaFile: -1, deltaRank: -1 },
+  { deltaFile: -1, deltaRank: 0 },
+  { deltaFile: 0, deltaRank: 1 },
+  { deltaFile: 1, deltaRank: -1 }
+]
 const king = Object.create(piece)
-king.getPossibleMoves = function() {}
+king.getPossibleMoves = function() {
+  var currentRank = parseInt(this.getRank())
+  var currentFileNumber = fileNameToNumberMap[this.getFile()]
+
+  var possibleMoves = []
+
+  movements.forEach(movement => {
+    let rank = currentRank + movement.deltaRank
+    let fileNumber = currentFileNumber + movement.deltaFile
+    let file = numberToFileNameMap[fileNumber]
+    if (isValidPosition(file, rank)) {
+      possibleMoves.push({
+        rank: rank,
+        file: file
+      })
+    }
+  })
+  return possibleMoves
+}
 
 export { king }
