f9a644b9c38f102b27f3645326051f3bb321983b
aruncs <acspayyeri@gmail.com>
2019-10-13 00:25:09 +0530

    queen movement

--- src/client/js/queen.js
+++ src/client/js/queen.js
@@ -1,6 +1,38 @@
 import { piece } from "./piece"
+import { numberToFileNameMap, fileNameToNumberMap, isValidPosition } from "./util"
+
+const movements = [
+  { deltaFile: 1, deltaRank: 1 },
+  { deltaFile: 1, deltaRank: 0 },
+  { deltaFile: -1, deltaRank: 1 },
+  { deltaFile: 0, deltaRank: -1 },
+  { deltaFile: -1, deltaRank: -1 },
+  { deltaFile: -1, deltaRank: 0 },
+  { deltaFile: 0, deltaRank: 1 },
+  { deltaFile: 1, deltaRank: -1 }
+]
 
 const queen = Object.create(piece)
-queen.getPossibleMoves = function() {}
+queen.getPossibleMoves = function() {
+  const currentFileNumber = fileNameToNumberMap[this.getFile()]
+  const currentRank = parseInt(this.getRank())
+  const possibleMoves = []
+
+  movements.forEach(movement => {
+    let rank = currentRank + movement.deltaRank
+    let fileNumber = currentFileNumber + movement.deltaFile
+    let file = numberToFileNameMap[fileNumber]
+    while (isValidPosition(file, rank)) {
+      possibleMoves.push({
+        file: file,
+        rank: rank
+      })
+      rank = rank + movement.deltaRank
+      fileNumber = fileNumber + movement.deltaFile
+      file = numberToFileNameMap[fileNumber]
+    }
+  })
+  return possibleMoves
+}
 
 export { queen }
